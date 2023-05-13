2a571a65a70326ed97b20e81b65b7306914fe198
Marc <teste74@hotmail.fr>
2023-05-13 18:37:02 +0200

    Update disk.sh

--- Proxmox/script/disk.sh
+++ Proxmox/script/disk.sh
@@ -1,7 +1,18 @@
+###########################################################################
+# Présentation:                                                           #
+# - Script pour l'ajout ou suppression de Disque via un menu de sélection #
+###########################################################################
+
+# Nettoyage de la console
 clear;
+
+# Chargement des fonctions Attach
 source fonction/attach
+
+# Déclaration d'une variable pour la boucle Infinie
 MENU=0
 
+# Boucle Infinie
 while [ $MENU = 0 ];
  do
  func_MENU;
